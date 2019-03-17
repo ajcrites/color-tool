d9208a33f015f52e3cf6a1c182f44a69b3f3468a
Andrew Crites <acrites@mobiquityinc.com>
2019-03-17 19:02:14 -0400

    feat(cleanup): add description / intro / header

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ColorToolApp } from './ColorToolApp';
+import { ColorTool } from '~/ColorTool';
 import './style.css';
 
-ReactDOM.render(<ColorToolApp />, document.getElementById('root'));
+ReactDOM.render(<ColorTool />, document.getElementById('root'));
