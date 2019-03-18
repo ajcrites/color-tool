7919a041b97787badab81f80ee11e1e61111f56c
Andrew Crites <acrites@mobiquityinc.com>
2019-03-17 20:29:06 -0400

    feat(cleanpu): allow intro to be collapsible and minor style improvements

--- src/color-check-util.ts
+++ src/color-check-util.ts
@@ -18,7 +18,7 @@ export function clampMultiColorValue(parser: 'rgba' | 'hsla', value, idx) {
   if (parser === 'rgba' && idx < 3) {
     maxClamp = 255;
   } else if (parser === 'hsla' && idx === 0) {
-    maxClamp = 360;
+    maxClamp = 359;
   } else if (parser === 'hsla' && idx > 0 && idx < 3) {
     maxClamp = 100;
   } else {
